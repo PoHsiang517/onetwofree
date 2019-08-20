b6ee006175d09313fdb8957c90a2629f166962a5
HsiangTW <boshian@gmail.com>
2019-08-20 22:36:40 +0800

    add你輸入的訊息是:V2

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = ""您輸入的訊息是: " + TextSendMessage(text=event.message.text)
+    message = "您輸入的訊息是: " + TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
